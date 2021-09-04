3c4f5ec00007106bfbb454c387124caa5595ca0d
xiangfeidexiaohuo <jsjson@163.com>
2021-09-05 02:04:46 +0800

    fix luci-app-cpulimit
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- luci-app-cpulimit/luasrc/controller/cpulimit.lua
+++ luci-app-cpulimit/luasrc/controller/cpulimit.lua
@@ -5,8 +5,9 @@ function index()
 	if not nixio.fs.access("/etc/config/cpulimit") then
 		return
 	end
-        entry({"admin", "system"}, firstchild(), "System", 44).dependent = false
-	local page = entry({"admin", "system", "cpulimit"}, cbi("cpulimit"), _("CPU limit"), 65)
+	
+	local page = entry({"admin", "system", "cpulimit"}, cbi("cpulimit"), luci.i18n.translate("cpulimit"), 65)
+	
 	page.dependent = true
 
 end
